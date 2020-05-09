4fce32b49512ef10a8595997d2d60202f6b23da9
Andy Revell <andyrevell93@gmail.com>
2020-05-09 01:56:48 -0400

    update iEEG get data and FC code. Added sheel script to mass produce data

--- pipelines/get_Functional_connectivity.py
+++ pipelines/get_Functional_connectivity.py
@@ -1,40 +1,53 @@
 """
 2020.05.06
 Andy Revell and Alex Silva
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Purpose:
     Calculate functional correlations between given time series data and channels.
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Logic of code:
     1. Calculate correlations within a given time window: Window data in 1 second
     2. Calculate broadband functional connectivity with echobase broadband_conn
     3. Calculate other band functional connectivity with echobase multiband_conn
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Input:
     inputfile: a pickled list. See get_iEEG_data.py and https://docs.python.org/3/library/pickle.html for more information
         index 0: time series data N x M : row x column : time x channels
         index 1: fs, sampling frequency of time series data
-
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Output:
-    outputfile: saves output as a numpy array npz. Array is organized by names of bands:
-    broadband_CC, alphatheta, beta, lowgamma, highgamma
+    Saves file outputfile as a pickel. For more info on pickeling, see https://docs.python.org/3/library/pickle.html
+    Briefly: it is a way to save + compress data. it is useful for saving lists, as in a list of time series data and sampling frequency together along with channel names
 
+    List index 0: ndarray. broadband. C x C x T. C: channels. T: times (1 second intervals). To change, see line 70: endInd = int(((t+1)*fs) - 1)
+    List index 1: ndarray. alphatheta. C x C x T
+    List index 2: ndarray. beta. C x C x T
+    List index 3: ndarray. lowgamma. C x C x T
+    List index 4: ndarray. highgamma. C x C x T
+    List index 5: ndarray. C x _  Electrode row and column names. Stored here are the corresponding row and column names in the matrices above.
+    List index 6: pd.DataFrame. N x 1. order of matrices in pickle file. The order of stored matrices are stored here to aid in transparency.
+        Typically, the order in broadband, alphatheta, beta, lowgamma, highgamma
+
+    To open the pickle file, use command:
+    with open(outputfile, 'rb') as f: broadband, alphatheta, beta, lowgamma, highgamma, electrode_row_and_column_names, order_of_matrices_in_pickle_file = pickle.load(f)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Example:
 
-inputfile = '/Users/andyrevell/Box/01_papers/sub-RID0278_HUP138_phaseII_415723190000_6000000.pickle'
-outputfile = '/Users/andyrevell/Box/01_papers/sub-RID0278_HUP138_phaseII_415723190000_6000000.npz'
-getFuncConn(inputfile,outputfile)
-
+inputfile = '/Users/andyrevell/mount/DATA/Human_Data/BIDS_processed/sub-RID0278/eeg/sub-RID0278_HUP138_phaseII_248432340000_248525740000_EEG.pickle'
+outputfile = '/Users/andyrevell/mount/DATA/Human_Data/BIDS_processed/sub-RID0278/connectivity_matrices/functional/sub-RID0278_HUP138_phaseII_248432340000_248525740000_functionalConnectivity.pickle'
+get_Functional_connectivity(inputfile,outputfile)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Please use this naming convention if data is from iEEG.org
-sub-RIDXXXX_iEEGFILENAME_STARTTIME_DURATION
-example: 'sub-RID0278_HUP138_phaseII_415723190000_6000000'
+sub-RIDXXXX_iEEGFILENAME_STARTTIME_STOPTIME_functionalConnectivity.pickle
+example: 'sub-RID0278_HUP138_phaseII_248432340000_248525740000_functionalConnectivity.pickle'
 
 """
 from echobase import broadband_conn, multiband_conn
 import numpy as np
 import pickle
 import pandas as pd
+
 """"
 Note on Pycharm users to import echobase:
 Right click the folder in which get_Functional_connectivity.py and echobase.py (MUST BE IN SAME DIRECTORY)
@@ -42,7 +55,10 @@ Select "Mark Directory As" --> "Mark as Source Root"
 Can also go to preferences --> Project structure to change source root 
 """
 
-def getFuncConn(inputfile,outputfile):
+def get_Functional_connectivity(inputfile,outputfile):
+    print("\n\nCalculating Functional Connectivity:")
+    print("inputfile: {0}".format(inputfile))
+    print("outputfile: {0}".format(outputfile))
     with open(inputfile, 'rb') as f: data, fs = pickle.load(f)#un-pickles files
     data_array = np.array(data)
     fs = float(fs)
@@ -54,8 +70,9 @@ def getFuncConn(inputfile,outputfile):
     highgamma = np.zeros((np.size(data_array,1),np.size(data_array,1),totalSecs))
     lowgamma = np.zeros((np.size(data_array,1),np.size(data_array,1),totalSecs))
     for t in range(0,totalSecs):
+        print("Calculating {0} out of {1} seconds".format(t+1, totalSecs))
         startInd = int(t*fs)
-        endInd = int(((t+1)*fs) - 1)
+        endInd = int(((t+1)*fs) - 1) #calculating over 1 second windows
         window = data_array[startInd:endInd,:]
         broad = broadband_conn(window,int(fs),avgref=True)
         adj_alphatheta, adj_beta, adj_lowgamma, adj_highgamma = multiband_conn(window,int(fs),avgref=True)
@@ -64,7 +81,9 @@ def getFuncConn(inputfile,outputfile):
         broadband[:,:,t] = broad
         highgamma[:,:,t] = adj_highgamma
         lowgamma[:,:,t] = adj_lowgamma
-        print(t)
-    np.savez(outputfile, broadband=broadband, alphatheta=alphatheta, beta=beta, lowgamma=lowgamma, highgamma=highgamma)
-
-
+    print("Saving to: {0}".format(outputfile))
+    electrode_row_and_column_names = data.columns.values
+    order_of_matrices_in_pickle_file = pd.DataFrame(["broadband", "alphatheta", "beta", "lowgamma" , "highgamma" ], columns=["Order of matrices in pickle file"])
+    with open(outputfile, 'wb') as f: pickle.dump([broadband, alphatheta, beta, lowgamma, highgamma, electrode_row_and_column_names, order_of_matrices_in_pickle_file], f)
+    #np.savez(outputfile, broadband=broadband, alphatheta=alphatheta, beta=beta, lowgamma=lowgamma, highgamma=highgamma)
+    print("...done\n\n")
--- pipelines/get_iEEG_data.py
+++ pipelines/get_iEEG_data.py
@@ -1,50 +1,63 @@
 """"
 2020.04.06. Python 3.7
 Andy Revell
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Purpose:
 To get iEEG data from iEEG.org. Note, you must download iEEG python package from GitHub - instructions are below
 1. Gets time series data and sampling frequency information. Specified electrodes are removed.
 2. Saves as a pickle format
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Input
     username: your iEEG.org username
     password: your iEEG.org password
     iEEG_filename: The file name on iEEG.org you want to download from
     start_time_usec: the start time in the iEEG_filename. In microseconds
-    duration: the duration of data you want to download. In microseconds
-    removed_channels: the channel names EXACT MATCH you want to exclude.
+    stop_time_usec: the stop time in the iEEG_filename. In microseconds.
+        iEEG.org needs a duration input: this is calculated by stop_time_usec - start_time_usec
+    ignore_electrodes: the electrode/channel names you want to exclude. EXACT MATCH on iEEG.org. Caution: some may be LA08 or LA8
     outputfile: the path and filename you want to save.
         PLEASE INCLUDE EXTENSION .pickle.
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Output:
     Saves file outputfile as a pickel. For more info on pickeling, see https://docs.python.org/3/library/pickle.html
-    Briefly: it is a way to save + compress data. it is useful for saving lists, as in a list of time series data and sampling frequency together
+    Briefly: it is a way to save + compress data. it is useful for saving lists, as in a list of time series data and sampling frequency together along with channel names
 
+    List index 0: Pandas dataframe. T x C (rows x columns). T is time. C is channels.
+    List index 1: float. Sampling frequency. Single number
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Example usage:
 
 username = 'arevell'
-password = ''
+password = 'password'
 iEEG_filename='HUP138_phaseII'
-start_time_usec = 415723190000
-duration = 6000000
+start_time_usec = 248432340000
+stop_time_usec = 248525740000
 removed_channels = ['EKG1', 'EKG2', 'CZ', 'C3', 'C4', 'F3', 'F7', 'FZ', 'F4', 'F8', 'LF04', 'RC03', 'RE07', 'RC05', 'RF01', 'RF03', 'RB07', 'RG03', 'RF11', 'RF12']
-outputfile = '/Users/andyrevell/Box/01_papers/sub-RID0278_HUP138_phaseII_415723190000_6000000.pickle'
+outputfile = '/Users/andyrevell/mount/DATA/Human_Data/BIDS_processed/sub-RID0278/eeg/sub-RID0278_HUP138_phaseII_248432340000_248525740000_EEG.pickle'
+get_iEEG_data(username, password, iEEG_filename, start_time_usec, stop_time_usec, removed_channels, outputfile)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+To run from command line:
+python3.6 -c 'import get_iEEG_data; get_iEEG_data.get_iEEG_data("arevell", "password", "HUP138_phaseII", 248432340000, 248525740000, ["EKG1", "EKG2", "CZ", "C3", "C4", "F3", "F7", "FZ", "F4", "F8", "LF04", "RC03", "RE07", "RC05", "RF01", "RF03", "RB07", "RG03", "RF11", "RF12"], "/gdrive/public/DATA/Human_Data/BIDS_processed/sub-RID0278/eeg/sub-RID0278_HUP138_phaseII_D01_248432340000_248525740000_EEG.pickle")'
 
-#Please use this output naming convention: sub-RIDXXXX_iEEGFILENAME_STARTTIME_DURATION.pickle
-#example: 'sub-RID0278_HUP138_phaseII_415723190000_6000000.pickle'
-
-get_iEEG_data(username, password, iEEG_filename, start_time_usec, duration, removed_channels, outputfile)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #How to get back pickled files
-with open(outputfile, 'rb') as f:
-    data, fs = pickle.load(f)
+with open(outputfile, 'rb') as f: data, fs = pickle.load(f)
 """
 from ieeg.auth import Session
 import pandas as pd
 import pickle
 
-def get_iEEG_data(username, password, iEEG_filename, start_time_usec, duration, ignore_electrodes, outputfile):
+def get_iEEG_data(username, password, iEEG_filename, start_time_usec, stop_time_usec, ignore_electrodes, outputfile):
+    print("\n\nGetting data from iEEG.org:")
+    print("iEEG_filename: {0}".format(iEEG_filename))
+    print("start_time_usec: {0}".format(start_time_usec))
+    print("stop_time_usec: {0}".format(stop_time_usec))
+    print("ignore_electrodes: {0}".format(ignore_electrodes))
+    print("Saving to: {0}".format(outputfile))
+    start_time_usec = int(start_time_usec)
+    stop_time_usec = int(stop_time_usec)
+    duration = stop_time_usec - start_time_usec
     s = Session(username, password)
     ds = s.open_dataset(iEEG_filename)
     channels = list(range(len(ds.ch_labels)))
@@ -53,6 +66,7 @@ def get_iEEG_data(username, password, iEEG_filename, start_time_usec, duration, 
     df = pd.DataFrame.drop(df, ignore_electrodes, axis=1)
     fs = ds.get_time_series_details(ds.ch_labels[0]).sample_rate #get sample rate
     with open(outputfile, 'wb') as f: pickle.dump([df, fs], f)
+    print("...done\n\n")
 
 
 """"
